47e9d8d878c46805a9dec18e443367459e86f9b4
crafts0 <gueyekallamoussa@gmail.com>
2024-02-22 09:24:46 +0000

    multiple of 5 and 3

---
+++ multipleof5Or3.go
@@ -0,0 +1,13 @@
+package projecteuler
+
+func MultipleOf5Or3(three, five, x int) int {
+	var sum int
+	var i = 1
+	for i < x {
+		if i%three == 0 || i%five == 0 {
+			sum += i
+		}
+		i++
+	}
+	return sum
+}
